d0534ffb3715d8c814f083c18c7125c2ce91561a
iagoduquedeveloper <iagoduquedeveloper@gmail.com>
2023-04-15 00:35:57 -0300

    first commit

---
+++ src/pages/detail/index.js
@@ -0,0 +1,18 @@
+import {View, Text, StyleSheet } from 'react-native'
+
+export function Detail(){
+    return(
+        <View style={styles.container}>
+            <Text>Página Detail!!  </Text>
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create(
+    {
+        container:{
+            backgroundColor: 'blue',
+        }
+    }
+)
---
+++ src/pages/search/index.js
@@ -0,0 +1,18 @@
+import {View, Text, StyleSheet } from 'react-native'
+
+export function Search(){
+    return(
+        <View style={styles.container}>
+            <Text>Página buscar!!  </Text>
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create(
+    {
+        container:{
+            backgroundColor: 'green',
+        }
+    }
+)
